c332d91dec6162f618157ddda9021fb25bbd47b9
刘祥超 <iwind.liu@gmail.com>
2021-01-20 08:59:04 +0800

    增加dbs.OnReadyDone()方法

--- dbs/ready.go
+++ dbs/ready.go
@@ -3,6 +3,7 @@ package dbs
 import "sync"
 
 var readyCallbacks = []func(){}
+var readyDoneCallbacks = []func(){}
 var readyLocker = sync.Mutex{}
 
 // 添加Ready的回调函数
@@ -14,10 +15,22 @@ func OnReady(f func()) {
 	readyLocker.Unlock()
 }
 
+// 添加Ready完成的回调函数
+func OnReadyDone(f func()) {
+	readyLocker.Lock()
+	if f != nil {
+		readyDoneCallbacks = append(readyDoneCallbacks, f)
+	}
+	readyLocker.Unlock()
+}
+
 // 调用Ready回调
 func NotifyReady() {
 	readyLocker.Lock()
 	for _, f := range readyCallbacks {
+		f()
+	}
+	for _, f := range readyDoneCallbacks {
 		f()
 	}
 	readyLocker.Unlock()
