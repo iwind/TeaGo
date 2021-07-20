fc255c995afa9bbd66b0d8c6fe39b71f8f0ad209
刘祥超 <iwind.liu@gmail.com>
2021-07-20 09:13:03 +0800

    RequestRemoteIP()支持IPv6

--- actions/action_object.go
+++ actions/action_object.go
@@ -8,6 +8,7 @@ import (
 	"github.com/iwind/TeaGo/logs"
 	"github.com/iwind/TeaGo/rands"
 	"github.com/iwind/TeaGo/types"
+	"net"
 	"net/http"
 	"strings"
 	"sync"
@@ -55,7 +56,7 @@ type ActionObject struct {
 	writer ActionWriter
 }
 
-// 取得内置的动作对象
+// Object 取得内置的动作对象
 func (this *ActionObject) Object() *ActionObject {
 	return this
 }
@@ -65,18 +66,18 @@ func (this *ActionObject) init() {
 	this.Data = map[string]interface{}{}
 }
 
-// 设置参数
+// SetParam 设置参数
 func (this *ActionObject) SetParam(name, value string) {
 	this.ParamsMap[name] = []string{value}
 }
 
-// 判断是否有参数
+// HasParam 判断是否有参数
 func (this *ActionObject) HasParam(name string) bool {
 	values, ok := this.ParamsMap[name]
 	return ok && len(values) > 0
 }
 
-// 获取参数
+// Param 获取参数
 func (this *ActionObject) Param(name string) (value string, found bool) {
 	values, ok := this.ParamsMap[name]
 	if !ok {
@@ -88,25 +89,25 @@ func (this *ActionObject) Param(name string) (value string, found bool) {
 	return values[0], true
 }
 
-// 获取字符串参数
+// ParamString 获取字符串参数
 func (this *ActionObject) ParamString(name string) string {
 	v, _ := this.Param(name)
 	return v
 }
 
-// 获取整型参数
+// ParamInt 获取整型参数
 func (this *ActionObject) ParamInt(name string) int {
 	v, _ := this.Param(name)
 	return types.Int(v)
 }
 
-// 获取int64参数
+// ParamInt64 获取int64参数
 func (this *ActionObject) ParamInt64(name string) int64 {
 	v, _ := this.Param(name)
 	return types.Int64(v)
 }
 
-// 获取参数数组
+// ParamArray 获取参数数组
 func (this *ActionObject) ParamArray(name string) []string {
 	values, ok := this.ParamsMap[name]
 	if !ok {
@@ -115,7 +116,7 @@ func (this *ActionObject) ParamArray(name string) []string {
 	return values
 }
 
-// 获取客户端的地址，有可能会包含端口
+// RequestRemoteAddr 获取客户端的地址，有可能会包含端口
 func (this *ActionObject) RequestRemoteAddr() string {
 	value := this.Request.Header.Get("X-Real-IP")
 	if len(value) > 0 {
@@ -135,21 +136,21 @@ func (this *ActionObject) RequestRemoteAddr() string {
 	return this.Request.RemoteAddr
 }
 
-// 获取客户端的地址
+// RequestRemoteIP 获取客户端的地址
 func (this *ActionObject) RequestRemoteIP() string {
 	addr := this.RequestRemoteAddr()
 	if len(addr) == 0 {
 		return ""
 	}
 
-	index := strings.Index(addr, ":")
-	if index >= 0 {
-		return addr[:index]
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return addr
 	}
-	return addr
+	return host
 }
 
-// 判断URL的Path部分是否包含任一前缀
+// HasPrefix 判断URL的Path部分是否包含任一前缀
 func (this *ActionObject) HasPrefix(prefix ...string) bool {
 	if len(prefix) == 0 {
 		return false
@@ -163,22 +164,22 @@ func (this *ActionObject) HasPrefix(prefix ...string) bool {
 	return false
 }
 
-// 设置头部信息
+// AddHeader 设置头部信息
 func (this *ActionObject) AddHeader(name, value string) {
 	this.ResponseWriter.Header().Add(name, value)
 }
 
-// 获取Header值
+// Header 获取Header值
 func (this *ActionObject) Header(name string) string {
 	return this.Request.Header.Get(name)
 }
 
-// 设置cookie
+// AddCookie 设置cookie
 func (this *ActionObject) AddCookie(cookie *http.Cookie) {
 	http.SetCookie(this.ResponseWriter, cookie)
 }
 
-// 设置能接收的最大数据（字节）
+// SetMaxSize 设置能接收的最大数据（字节）
 func (this *ActionObject) SetMaxSize(maxSize float64) {
 	this.maxSize = maxSize
 }
@@ -188,7 +189,7 @@ func (this *ActionObject) Error(error string, code int) {
 	http.Error(this.ResponseWriter, error, code)
 }
 
-// 输出内容
+// WriteString 输出内容
 func (this *ActionObject) WriteString(output ...string) {
 	for _, outputArg := range output {
 		this.Write([]byte(outputArg))
@@ -204,7 +205,7 @@ func (this *ActionObject) Write(bytes []byte) {
 	}
 }
 
-// 输出可以格式化的内容
+// WriteFormat 输出可以格式化的内容
 func (this *ActionObject) WriteFormat(format string, args ...interface{}) {
 	if len(args) > 0 {
 		format = fmt.Sprintf(format, args...)
@@ -212,7 +213,7 @@ func (this *ActionObject) WriteFormat(format string, args ...interface{}) {
 	this.Write([]byte(format))
 }
 
-// 写入JSON
+// WriteJSON 写入JSON
 func (this *ActionObject) WriteJSON(value interface{}) {
 	this.ResponseWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
 
@@ -224,7 +225,7 @@ func (this *ActionObject) WriteJSON(value interface{}) {
 	this.Write(jsonBytes)
 }
 
-// 成功返回
+// Success 成功返回
 func (this *ActionObject) Success(message ...string) {
 	if len(message) > 0 {
 		this.Message = message[0]
@@ -269,7 +270,7 @@ func (this *ActionObject) Success(message ...string) {
 	panic(this)
 }
 
-// 失败返回
+// Fail 失败返回
 func (this *ActionObject) Fail(message ...string) {
 	if len(message) > 0 {
 		this.Message = strings.Join(message, "")
@@ -279,7 +280,7 @@ func (this *ActionObject) Fail(message ...string) {
 	panic(this)
 }
 
-// 字段错误提示
+// FailField 字段错误提示
 func (this *ActionObject) FailField(field string, message ...string) {
 	if len(message) > 0 {
 		this.Message = message[0]
@@ -324,17 +325,17 @@ func (this *ActionObject) failWithoutPanic() {
 	this.Write(jsonBytes)
 }
 
-// 设置Session管理器
+// SetSessionManager 设置Session管理器
 func (this *ActionObject) SetSessionManager(sessionManager interface{}) {
 	this.SessionManager = sessionManager
 }
 
-// 设置Session Cookie名称
+// SetSessionCookieName 设置Session Cookie名称
 func (this *ActionObject) SetSessionCookieName(cookieName string) {
 	this.sessionCookieName = cookieName
 }
 
-// 读取Session
+// Session 读取Session
 func (this *ActionObject) Session() *Session {
 	if this.session != nil {
 		return this.session
@@ -372,22 +373,22 @@ func (this *ActionObject) Session() *Session {
 	return session
 }
 
-// 设置模板目录
+// ViewDir 设置模板目录
 func (this *ActionObject) ViewDir(viewDir string) {
 	this.viewDir = viewDir
 }
 
-// 设置模板文件
+// View 设置模板文件
 func (this *ActionObject) View(viewTemplate string) {
 	this.viewTemplate = viewTemplate
 }
 
-// 设置模板文件中可以使用的自定义函数
+// ViewFunc 设置模板文件中可以使用的自定义函数
 func (this *ActionObject) ViewFunc(name string, f interface{}) {
 	this.viewFuncMap[name] = f
 }
 
-// 显示模板
+// Show 显示模板
 func (this *ActionObject) Show() {
 	this.AddHeader("Content-Type", "text/html; charset=utf-8")
 
@@ -398,7 +399,7 @@ func (this *ActionObject) Show() {
 	}
 }
 
-// 取得单个文件
+// File 取得单个文件
 func (this *ActionObject) File(field string) *File {
 	for _, file := range this.Files {
 		if file.Field == field {
@@ -408,7 +409,7 @@ func (this *ActionObject) File(field string) *File {
 	return nil
 }
 
-// 设置下一个动作
+// Next 设置下一个动作
 func (this *ActionObject) Next(nextAction string, params map[string]interface{}, hash ...string) *ActionObject {
 	this.next.Action = nextAction
 	this.next.Params = params
@@ -418,23 +419,23 @@ func (this *ActionObject) Next(nextAction string, params map[string]interface{},
 	return this
 }
 
-// 设置刷新
+// Refresh 设置刷新
 func (this *ActionObject) Refresh() *ActionObject {
 	this.next.Action = "*refresh"
 	return this
 }
 
-// 跳转
+// RedirectURL 跳转
 func (this *ActionObject) RedirectURL(url string) {
 	http.Redirect(this.ResponseWriter, this.Request, url, http.StatusTemporaryRedirect)
 }
 
-// 缓存
+// Cache 缓存
 func (this *ActionObject) Cache() *caches.Factory {
 	return this.Spec.Cache()
 }
 
-// 输出格式化后的JSON
+// Pretty 输出格式化后的JSON
 func (this *ActionObject) Pretty() *ActionObject {
 	this.pretty = true
 	return this
