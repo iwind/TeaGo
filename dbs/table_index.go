5534da0db9d1c8c622d52bb2342c9e53c257a865
刘祥超 <iwind.liu@gmail.com>
2021-08-06 13:44:28 +0800

    TableIndex.Definition()增加Unique Key支持

--- dbs/table_index.go
+++ dbs/table_index.go
@@ -10,11 +10,15 @@ type TableIndex struct {
 	Comment     string
 }
 
-func (index *TableIndex) Definition() string {
+func (this *TableIndex) Definition() string {
 	//示例：KEY `adId` (`adId`) USING BTREE COMMENT '广告ID'
-	pieces := []string{"KEY", "`" + index.Name + "`", "(`" + strings.Join(index.ColumnNames, "`,`") + "`)", "USING " + index.Type}
-	if len(index.Comment) > 0 {
-		pieces = append(pieces, "COMMENT '"+index.Comment+"'")
+	var pieces = []string{}
+	if this.IsUnique {
+		pieces = append(pieces, "UNIQUE")
+	}
+	pieces = append(pieces, "KEY", "`"+this.Name+"`", "(`"+strings.Join(this.ColumnNames, "`,`")+"`)", "USING "+this.Type)
+	if len(this.Comment) > 0 {
+		pieces = append(pieces, "COMMENT '"+this.Comment+"'")
 	}
 	return strings.Join(pieces, " ")
 }
